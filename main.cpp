2a82657cefe712d73a312b0aa05a8b458f1d2659
Paul Alvarez <palva002@well.cs.ucr.edu>
2014-10-18 14:36:32 -0700

    username added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include <string.h>
 using namespace std;
 
@@ -6,16 +7,27 @@ int main()
 {
  char *argv[100];
  char *pch;
+ char *usrname= getlogin();
  char user[50];
- cout<<"$ ";
+ cout<<usrname<<"@"<<"$ ";
  cin.getline(user,50);
  pch=strtok(user," ");
+ cout<<pch<<endl;
  for(int i=0;pch != NULL;i++)
  {
+   if(strcmp(pch,"exit")==0)
+   {
+     exit(0);
+   }
    argv[i]=new char[100];
    strcpy(argv[i],pch);
+    cout<<i<<endl; 
    pch=strtok(NULL," ");
  }
- execvp(argv[0],argv);
-
+  execvp(argv[0],argv);
+ 
+  for(int i=0;i<100;i++)
+  {
+    delete argv[i];
+  }
 }
