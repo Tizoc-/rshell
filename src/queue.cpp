5ab20baa9a80ee1c5598d583f0d5954b1bbe30cd
Paul Alvarez <palva002@hammer.cs.ucr.edu>
2014-11-15 22:10:26 -0800

    simple i/o

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -19,7 +21,7 @@ int pid;
     getline(cin,usr); 
     typedef boost::tokenizer<boost::char_separator<char> > 
         tokenizer;
-    boost::char_separator<char> sep(" ", ";&|");
+    boost::char_separator<char> sep(" ", ";&|<>");
     tokenizer tokens(usr, sep);
     for (tokenizer::iterator tok_iter = tokens.begin();
             tok_iter != tokens.end(); ++tok_iter)
@@ -30,9 +32,8 @@ int pid;
 
 
     char **argv=(char**)alloca(arg.size()+1);
-    cout<<count;
 
-    std::cout << "myqueue contains: ";
+    std::cout << "myqueue contains: "<<endl;
     while (!(arg.empty()))
     {
         if(arg.front()==";")
@@ -74,10 +75,72 @@ int pid;
                 count=0;
             }
         }
-        else if(arg.front()=="|")
-        {
+        else if(arg.front()==">")
+        { cout<<"\nredirect"<<endl;
+            arg.pop();
+            argv[count]=NULL;
+            pid = fork();
+            if(pid == -1){
+                perror("fork");
+            }
+            else if(pid == 0)
+            {
 
-        
+                int fd=open(arg.front().c_str(),O_RDWR|O_CREAT,0666);
+                if(fd==-1)
+                {
+                    perror("open");
+                    exit(1);
+                }
+            //    int oldstdout=dup(1);
+                close(1);
+                dup(fd);
+            //    cout << "fd=" << fd << endl;
+                if(execvp(argv[0], argv) == -1)
+                {
+                    perror("execvp");
+                    exit(1);
+                } 
+                else
+                {
+                    exit(1);
+                }
+              //  close(1);
+              //  dup(oldstdout);
+              //  cout << "to the screen" << endl;
+                /* cout<<"pid"<<endl;
+                   cout<<arg.front()<<endl;
+                   cout<<"l"<<endl;
+                   int fdo = open("testfile", O_RDWR|O_CREAT,0666);
+
+                   cout << "fdo: " << fdo << endl;
+                   if(fdo == -1)
+
+                   int oldstout=dup(1);
+                   if(oldstout==-1)
+                   {
+                   perror("dup");
+                   }
+                   if(close(1) == -1 )
+                   {
+                   perror("close");
+                   }
+                   if(dup(fdo) == -1)
+                   {
+                   perror("dup");
+                   }
+                   cout<<fdo<<endl;*/
+
+            }
+            else{
+                wait(NULL);
+                for(int i=0;i<count;i++)
+                {
+                    argv[i]==NULL;
+                }
+
+            }
+
         }
         else
         {
